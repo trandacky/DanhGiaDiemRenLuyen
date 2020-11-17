dff2bf214e7d5f3b0cf17ca4eda0f63489d56c1f
TranDacKy <trandackydbz2@gmail.com>
2020-11-17 16:44:46 +0700

    second commit

--- Diem_Ren_Luyen/src/main/java/com/example/demo/entity/BoCauHoi.java
+++ Diem_Ren_Luyen/src/main/java/com/example/demo/entity/BoCauHoi.java
@@ -22,7 +22,7 @@ public class BoCauHoi {
 	@Column(name = "tenBoCauHoi")
 	private String tenBoCauhoi;
 
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "boCauHoi")
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "boCauHoi", cascade = CascadeType.ALL)
 	private List<CauHoi> cauHoi;
 
 	public long getIdBoCauHoi() {
