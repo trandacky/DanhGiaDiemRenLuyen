ad48fcfbf4f19ae352e9becca72e3e5a3e005a95
thuhienng <nnguyenthithuhien.1999@gmail.com>
2020-12-15 21:53:42 +0700

    danhgialan2

--- Diem_Ren_Luyen/src/main/java/com/example/demo/service/PhieuRenLuyenService.java
+++ Diem_Ren_Luyen/src/main/java/com/example/demo/service/PhieuRenLuyenService.java
@@ -1,5 +1,16 @@
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.entity.BoCauHoi;
+import com.example.demo.entity.PhieuRenLuyen;
+
 public interface PhieuRenLuyenService {
 
+	List<PhieuRenLuyen> getAll();
+	Optional<PhieuRenLuyen> getByID(long id);
+	PhieuRenLuyen setData(PhieuRenLuyen phieuRenLuyen);
+	Optional<PhieuRenLuyen> update(PhieuRenLuyen phieuRenLuyen);
+	Optional<Object> delete(Long id);
 }
